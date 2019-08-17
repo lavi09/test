ec4b18e8a88238021e939e6e8c09813e88d22ecc
lavi09 <42918272+lavi09@users.noreply.github.com>
2019-08-17 11:25:04 -0700

    added a

--- chocolates/Program.cs
+++ chocolates/Program.cs
@@ -8,8 +8,33 @@ namespace chocolates
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static public void Main()
         {
+
+            int a = 10;
+            int l = 5;
+            int[] ar = { 8, 7, 6, 3, 9 };
+            int start = 0, end = l - 1;
+
+            while (ar[start] != ar[end])
+            {
+                if (ar[start] > ar[end])
+                {
+                    start++;
+                }
+                else if (ar[start] < ar[end])
+                {
+                    end--;
+                }
+
+            }
+
+
+
+            Console.WriteLine("{0}", ar[start]);
+            Console.ReadLine();
+
         }
     }
 }
